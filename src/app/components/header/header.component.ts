e12633ec636d32dabfd5aa91e4e65d203d736533
Adam <adamcekala1@hotmail.com>
2017-09-04 22:21:01 +0200

    refactor styles, navbar. Refactor project. Start again. Simple template.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,38 +1,11 @@
-import {Component, HostListener, OnInit} from '@angular/core';
-import {Window} from 'selenium-webdriver';
+import {Component} from '@angular/core';
 import 'rxjs/add/operator/debounceTime';
-import {Subject} from "rxjs/Subject";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  isCollapsedParam: boolean = true;
-  screenWidth: Subject<number> = new Subject();
-  private widthScreen: number = window.innerWidth;
+export class HeaderComponent{
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.screenWidth.next(event.target.innerWidth);
-  }
-
-  constructor() { }
-
-  ngOnInit() {
-    if(this.widthScreen > 768) {
-      this.isCollapsedParam = false;
-    }
-
-    this.screenWidth.debounceTime(100).subscribe((width: number) => {
-      if (width > 768) {
-        this.isCollapsedParam = false;
-      }
-    });
-  }
-
-  toggleCollapse() {
-    this.isCollapsedParam = !this.isCollapsedParam;
-  }
 }
