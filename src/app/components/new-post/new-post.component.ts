b1d96dda55f67e9633771432fa489ac45fb06c35
Adam <adamcekala1@hotmail.com>
2017-09-23 20:38:43 +0200

    add images to local storage, added conenction beetwen thumbnails and local storage

--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -1,8 +1,12 @@
+import * as firebase from 'firebase/app';
 import * as moment from 'moment';
 import {AlertService} from 'ngx-alerts';
 import {AngularFireDatabase} from 'angularfire2/database';
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ModalDirective} from 'ngx-bootstrap';
+
+import {CONSTANTS} from '../../shared/CONSTANTS';
 import {HttpService} from '../../core/http/http.service';
 import {InputFile} from '../../core/http/http.interface';
 
@@ -14,8 +18,7 @@ import {InputFile} from '../../core/http/http.interface';
 export class NewPostComponent implements OnInit {
   quickCreatePost: FormGroup;
   uploadImage: {file?: InputFile, url?: string, error?: string} = {};
-  uploadImageUrl: string;
-  uploadImageError: string;
+  @ViewChild('createNewPostModal') createNewPostModal: ModalDirective;
 
   constructor(private alertService: AlertService,
               private angularFireDatabase: AngularFireDatabase,
@@ -23,48 +26,56 @@ export class NewPostComponent implements OnInit {
               private httpService: HttpService) {}
 
   createNewPost() {
-    const {title, content} = this.quickCreatePost.value;
-
-
     if(this.uploadImage.file) {
       this.httpService.saveImgToStorageFromInput(this.uploadImage.file).then((response: { fileUrl?: string, error?: string }) => {
         if (response.fileUrl) {
           this.uploadImage.url = response.fileUrl;
+          this.postData();
         } else {
-          this.uploadImageError = response.error;
+          this.uploadImage.error = response.error;
         }
-
-        this.angularFireDatabase.list('posts').push({
-          authorId: 1,
-          content,
-          date: moment().format('DD/MM/YYYY HH:mm:ss'),
-          lastUpdate: '',
-          thumbnail: this.uploadImage.url || '',
-          title
-        });
       });
     } else {
-      this.angularFireDatabase.list('posts').push({
-        authorId: 1,
-        content,
-        date: moment().format('DD/MM/YYYY HH:mm:ss'),
-        lastUpdate: '',
-        thumbnail: this.uploadImage.url || '',
-        title
-      });
+      this.postData();
     }
   }
 
   readInputFile(event) {
-    if (event.target.files && event.target.files[0]) {
-      this.uploadImage.file = event.target.files[0];
+    if(event.target.files && event.target.files[0]) {
+      const file: InputFile = event.target.files[0];
+
+      if (file.size < CONSTANTS.MAX_IMAGES_SIZE && file.type.slice(0, 5) === 'image') {
+        this.uploadImage.file = event.target.files[0];
+      } else {
+        this.quickCreatePost.get('file').reset();
+        this.alertService.danger('Plik za duży lub zły format.');
+      }
     }
+  }
+
+  clearInputs() {
+    delete this.uploadImage.file;
+    this.quickCreatePost.reset();
   }
 
   ngOnInit() {
     this.quickCreatePost = this.formBuilder.group({
       title: ['', Validators.required ],
-      content: ['', Validators.required ]
+      content: ['', Validators.required ],
+      file: ''
     });
+  }
+
+  private postData() {
+    const {title, content} = this.quickCreatePost.value;
+
+    return this.angularFireDatabase.list('posts').push({
+      authorId: 1,
+      content,
+      date: moment().format('DD/MM/YYYY HH:mm:ss'),
+      lastUpdate: '',
+      thumbnail: this.uploadImage.url || '',
+      title
+    }).then(() => this.createNewPostModal.hide(), (err) => this.alertService.danger(err.toString()));
   }
 }
